77b5f6b80d276efa108bc792f380fb2b4e35d3c3
Justin Holt <jsholt42@students.tntech.edu>
2018-06-19 20:39:56 -0500

    canvas and background colors inverted

---
+++ canvas.js
@@ -0,0 +1,6 @@
+//searches entire document and finds canvas variable
+var canvas = document.querySelector('canvas');
+
+//set width and height of canvas to the width of the browser screen
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
