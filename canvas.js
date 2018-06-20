ed7a06e33d108ee262b0598aedbe1888bf57b58a
Justin Holt <jsholt42@students.tntech.edu>
2018-06-20 02:10:23 -0500

    update canvas.js, removed inner borders, object oriented star sprite

--- canvas.js
+++ canvas.js
@@ -12,11 +12,47 @@ var c = canvas.getContext('2d');
 var leftWidth = (canvas.width / 3) * 2;
 var rightWidth = (canvas.width / 3);
 
-c.beginPath();
-c.strokeStyle = "red";
-c.lineWidth = "1";
-//left rectangle
-c.rect(10, 10, leftWidth, canvas.height - 20);
-//right rectangle
-c.rect(leftWidth + 20, 10, rightWidth - 30, canvas.height - 20 )
-c.stroke();
+// c.beginPath();
+// c.strokeStyle = "red";
+// c.lineWidth = "1";
+// //left rectangle
+// c.rect(10, 10, leftWidth, canvas.height - 20);
+// //right rectangle
+// c.rect(leftWidth + 20, 10, rightWidth - 30, canvas.height - 20 )
+// c.stroke();
+
+function Star(x, y, dy) 
+{
+    this.x = x;
+    this.y = y;
+    this.dy = dy;
+
+    this.draw = function() 
+    {
+        c.fillRect(x, y, 6, 6);
+    }
+
+    this.update = function()
+    {
+        if (this.y >= innerHeight) {
+            this.y = 10;
+            this.x = Math.floor(Math.random() * leftWidth);
+            this.dy = Math.floor((Math.random() * 5) + 3);
+        }
+    }
+}
+
+var star = new Star(Math.floor(Math.random() * leftWidth), Math.floor(Math.random() * innerHeight), Math.floor((Math.random() * 5) + 3));
+
+var x = Math.floor(Math.random() * leftWidth);
+var y = Math.floor(Math.random() * innerHeight);
+var dy = Math.floor((Math.random() * 5) + 3);
+function starMovement() 
+{
+    requestAnimationFrame(starMovement);
+    c.clearRect(0,0,innerWidth,innerHeight);
+
+    star.draw();
+}
+
+starMovement();
