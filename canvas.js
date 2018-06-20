f33743ca700b36d3e235865e2dfebc0b098e6feb
Justin Holt <jsholt42@students.tntech.edu>
2018-06-20 17:28:44 -0500

    complete starfield, add ship and event handlers

--- canvas.js
+++ canvas.js
@@ -3,11 +3,40 @@ var canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth - 2;
 canvas.height = window.innerHeight - 2;
 
+//graphics getContext
 var c = canvas.getContext('2d');
 
 //create two windows inside the canvas, one for the game and one to store player data
 var leftWidth = (canvas.width / 3) * 2;
 var rightWidth = (canvas.width / 3);
+
+var mousePosition = {
+    x: 0,
+    y: 0
+}
+
+function eventHandler(e)
+{
+    mousePosition.x = e.x;
+    mousePosition.y = e.y;
+}
+
+window.addEventListener("mousemove", eventHandler, false);
+
+function Ship(x, y, dx) {
+    this.dx = dx;
+    this.y = innerHeight - 20;
+
+    this.draw = function()
+    {
+        c.fillRect(this.x, this.y, 6, 6);
+    }
+
+    this.update = function()
+    {
+        this.x = mousePosition.x;
+    }
+}
 
 function Star(x, y, dy, size) 
 {
@@ -17,7 +46,7 @@ function Star(x, y, dy, size)
 
     this.draw = function() 
     {
-        c.fillRect(this.x, this.y, size, size);
+        c.fillRect(this.x, this.y, this.size, this.size);
     }
 
     this.update = function()
@@ -33,18 +62,28 @@ function Star(x, y, dy, size)
     }
 }
 
+var ship = new Ship(rightWidth / 2);
 var stars = [];
 for (var i = 0; i < 50; ++i) {
     stars.push(new Star(Math.floor(Math.random() * leftWidth), Math.floor(Math.random() * innerHeight), Math.floor((Math.random() * 3) + 3), Math.floor(Math.random() * 4)));
 }
 
+
+
+/****** SPRITE ANIMATION ******/
 function starMovement() 
 {
     requestAnimationFrame(starMovement);
-    c.clearRect(0,0,innerWidth,innerHeight);
+    c.clearRect(0, 0, innerWidth, innerHeight);
     for (var i = 0; i < stars.length; ++i ) {
         stars[i].update();
     }
+}
+
+function shipMovement() {
+    requestAnimationFrame(shipMovement);
+    c.clearRect(0, 0, innerWidth, innerHeight);
+    ship.update();
 }
 
 starMovement();
