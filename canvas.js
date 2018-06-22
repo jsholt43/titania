547f17c4655bc932ab9db5d55ce84fc656840ed1
Justin Holt <jsholt42@students.tntech.edu>
2018-06-21 20:50:02 -0500

    finish separating files

--- canvas.js
+++ canvas.js
@@ -33,28 +33,28 @@ var keys = {
 //     rightWidth = (canvas.width / 3);
 // }
 
-function event_KeyPress(e)
-{
-    console.log(e.which);
-    switch(e.which) {
-        case 32:
-            console.log("space");
-            keys.space = true;
-            break;
-        case 97: 
-            console.log("A");
-            keys.a = true;
-            break;
-        case 100: 
-            console.log("D");
-            keys.d = true;
-            break;
-        case 112:
-            console.log("P");
-            keys.p = !keys.p;
-            break;
-    }
-}
+// function event_KeyPress(e)
+// {
+//     console.log(e.which);
+//     switch(e.which) {
+//         case 32:
+//             console.log("space");
+//             keys.space = true;
+//             break;
+//         case 97: 
+//             console.log("A");
+//             keys.a = true;
+//             break;
+//         case 100: 
+//             console.log("D");
+//             keys.d = true;
+//             break;
+//         case 112:
+//             console.log("P");
+//             keys.p = !keys.p;
+//             break;
+//     }
+// }
 
 // function event_MouseOut(e)
 // {
