2e949c0a2d029443aabc307031e4d27d3a83fd78
Justin Holt <jsholt42@students.tntech.edu>
2018-06-21 22:02:35 -0500

    add event_handlers.js for organization

---
+++ js/event_handlers.js
@@ -0,0 +1,22 @@
+function event_KeyPress(e)
+{
+    console.log(e.which);
+    switch(e.which) {
+        case 32:
+            console.log("space");
+            keys.space = true;
+            break;
+        case 97: 
+            console.log("A");
+            keys.a = true;
+            break;
+        case 100: 
+            console.log("D");
+            keys.d = true;
+            break;
+        case 112:
+            console.log("P");
+            keys.p = !keys.p;
+            break;
+    }
+}
