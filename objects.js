241444c80048a14fad0690fc3cb07faa4e8336d1
Justin Holt <jsholt42@students.tntech.edu>
2018-06-21 20:48:20 -0500

    add separate files to sort code

---
+++ objects.js
@@ -0,0 +1,61 @@
+function Ship(x, y, dx, size) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.size = size;
+
+    this.draw = function()
+    {
+        c.fillRect(this.x, this.y, this.size, this.size);
+    }
+
+    this.update = function()
+    {
+        if (keys.a == true) {
+            this.x -= 10;
+            keys.a = !keys.a;
+        } else if (keys.d == true) {
+            this.x += 10;
+            keys.d = !keys.d;
+        }
+
+        if (this.x <= 0) {
+            this.x = 0;
+        } else if (this.x >= leftWidth) {
+            this.x = leftWidth;
+        }
+        if (this.size >= 20) {
+
+        } else if (this.size <= 15) {
+
+        }
+        this.y = (innerHeight - this.size - 5);
+        this.size = leftWidth / 20;
+        console.log(this.size);
+        this.draw();
+    }
+}
+
+function Star(x, y, dy, size) 
+{
+    this.x = x;
+    this.y = y;
+    this.dy = dy;
+
+    this.draw = function() 
+    {
+        c.fillRect(this.x, this.y, this.size, this.size);
+    }
+
+    this.update = function()
+    {
+        if (this.y >= innerHeight) {
+            this.y = 10;
+            this.x = Math.floor(Math.random() * leftWidth);
+            this.dy = Math.floor((Math.random() * 2) + 5);
+            this.size = Math.floor(Math.random() * 4);
+        }
+        this.y += this.dy;
+        this.draw();
+    }
+}
